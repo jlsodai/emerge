8ef9224a73df7c393a673822b691074966c409d5
Julius A. Odai <jlsodai@gmail.com>
2024-04-10 10:17:30 +0000

    update grammatical errors, add application URL in FAQs page

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,11 +1,11 @@
 export const faqs = `
 ### How do I apply and when is the deadline for submission?
 
-Applications will be open from **Friday 19 April 2024, to Friday 10 May 2024**. You may submit your application through
+Applications will be open from **Friday 19 April 2024, to Friday 10 May 2024**. You may submit your application through [this link](https://emergeapplication.paperform.co/)
 
 ### Do I need to pay to participate in EMERGE?
 
-EMERGE is offered at no cost to eligible participants through our partnership with The Mastercard Foundation. There are no associated application, registration, or program fees. Participants should however have their own internet-connected device and ensure reliable internet access, as the program is conducted online. Additionally, all learning activities are conducted on the Zoom platform, so participants should have access to Zoom for seamless participation in the program's workshops and sessions.
+EMERGE is offered at no cost to eligible participants through our partnership with The Mastercard Foundation. There are no associated application, registration, or programme fees. Participants should however have their own internet-connected device and ensure reliable internet access, as the programme is conducted online. Additionally, all learning activities are conducted on the Zoom platform, so participants should have access to Zoom for seamless participation in the programme's workshops and sessions.
 
 ### How do I know if I am eligible to apply for EMERGE?
 
@@ -13,7 +13,7 @@ You are eligible to apply for the EMERGE programme if you are/have:
 
 - 25 - 35 years old
 - Completed a university degree
-- Have a good understanding of the English language (the program will be delivered in English)
+- Have a good understanding of the English language (the programme will be delivered in English)
 - 3+ years of professional work experience
 - An African national
 - Part of the Mastercard Foundation Alumni Network
@@ -22,7 +22,7 @@ You are eligible to apply for the EMERGE programme if you are/have:
 
 Once you submit your application, you will receive an email confirming receipt of the same. A representative will then respond within 15 business days to notify you of the status of your application. If successful, you will be enrolled on the online platform and receive further details of the programme components including dates and deadlines.
 
-If you have not received any emails from EMERGE within 15 working days of submitting your application, please check your spam folder. If you do not find the email, please get in touch with us via Add email.
+If you have not received any emails from EMERGE within 15 working days of submitting your application, please check your spam folder. If you do not find the email, please get in touch with us via info@emergeleaders.com
 
 Please ensure you provide a valid email address in your application registration form and/or your application, as it will be our primary way to contact you.
 
@@ -38,11 +38,11 @@ Participants will be required to commit approximately 6 hours each month to atte
 
 Programme participants will receive a certificate of completion at the end of the programme.
 
-Please note that TheBoardroom Africa is not an accredited educational institution, and thus, participants of the EMERGE program will not receive an accredited qualification or credential.
+Please note that TheBoardroom Africa is not an accredited educational institution, and thus, participants of the EMERGE programme will not receive an accredited qualification or credential.
 
 ### What makes EMERGE different from other leadership development programmes?
 
-EMERGE stands out from other leadership development programs due to its holistic approach. We go beyond simply imparting skills; we prioritise your overall growth and development. Our program combines practical skill-building with an in-depth exploration of key topics like Organisational Diversity and ESG, ensuring that you not only enhance your leadership capabilities but also gain a deep understanding of crucial issues facing modern leaders. Additionally, our personalised mentorship, interactive masterclasses, and vibrant community engagement create a supportive and dynamic learning environment tailored to your specific needs and aspirations.
+EMERGE stands out from other leadership development programmes due to its holistic approach. We go beyond simply imparting skills; we prioritise your overall growth and development. Our programme combines practical skill-building with an in-depth exploration of key topics like Organisational Diversity and ESG, ensuring that you not only enhance your leadership capabilities but also gain a deep understanding of crucial issues facing modern leaders. Additionally, our personalised mentorship, interactive masterclasses, and vibrant community engagement create a supportive and dynamic learning environment tailored to your specific needs and aspirations.
 
 ### Is this programme suitable for entrepreneurs?
 
