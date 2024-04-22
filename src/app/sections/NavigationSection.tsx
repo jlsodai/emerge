82f6f2bf6ad4f9dab2a82f31833750f8f11dbb1a
Julius A. Odai <jlsodai@gmail.com>
2024-04-22 07:52:54 +0000

    update logo area

--- src/app/sections/NavigationSection.tsx
+++ src/app/sections/NavigationSection.tsx
@@ -18,26 +18,26 @@ const NavigationSection = ({ mcard = false }) => {
   return (
     <>
       <section className="z-30 relative">
-        <div className="container flex justify-between py-6 items-center">
+        <div className="container flex justify-between py-6 items-center gap-4">
           <Link href="/">
             <img
               src="https://res.cloudinary.com/dhhw72iwq/image/upload/v1712736995/EMERGELogo_vyovkw.png"
-              className="w-[200px] md:w-[250px] -mt-2"
+              className="w-[130px] md:w-[250px] md:-mt-2"
               alt=""
             />
           </Link>
           <Navigation links={links} />
           {mcard ? (
-            <div className="flex gap-8 items-center">
+            <div className="flex gap-4 items-center">
               <div className="flex flex-col justify-center items-center">
                 <img
                   src="https://res.cloudinary.com/dhhw72iwq/image/upload/v1713446626/mcPartnership_zyzbaj.svg"
-                  className="w-[100px] mb-2"
+                  className="w-[60px] md:w-[100px] mb-2"
                   alt=""
                 />
                 <img
                   src="https://res.cloudinary.com/dhhw72iwq/image/upload/v1712579874/mastercard_o0bpqk.svg"
-                  className="w-[90px]"
+                  className="w-[50px] md:w-[90px]"
                   alt=""
                 />
               </div>
